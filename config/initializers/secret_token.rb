ad7d357a6eac9563801fd4157775b0f980e7e1a9
johndavid400 <jd@isotope11.com>
2013-06-05 13:25:53 -0500

    initial commit for spree 2.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JcobbDesigns::Application.config.secret_token = '5a4207807cddbd8e12c67b7bc0caef4fffa0049b8e0ac601d622b924be8a886c018d2e8872ab9641c77692621f22677735cb6bcca06886c719bf3037d8262954'
